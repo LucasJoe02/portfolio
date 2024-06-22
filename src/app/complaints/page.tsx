365c7291cf6dc803d0980f19273aab6d181f423a
Lucas Redding <lre61@uclive.ac.nz>
2024-06-23 00:53:08 +1200

    Fixed text colour on dark mode and video size on mobile

--- src/app/complaints/page.tsx
+++ src/app/complaints/page.tsx
@@ -1,21 +1,15 @@
-import styles from "../page.module.css"
+import styles from "./page.module.css"
 import React from "react"
 
 export default function Page() {
     return (
-      <div className={styles.container}>
-        <div style={{ height: '50vh', width: '50vw'}}>
-          <iframe
-            src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
-            style={{
-              border: 'none',
-              height: '100%',
-              width: '100%',
-            }}
-            allowFullScreen
-            title="Rolled"
-          />
-        </div>
+      <div className={styles.video}>
+        <iframe
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Rolled"
+        />
       </div>
     )
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import styles from './page.module.css'
 
 export default function Page() {
   return (
-    <div className={styles.container}>
-      <h1>Lucas Redding</h1>
-    </div>
+      <h1 className={styles.title}>Lucas Redding</h1>
   )
 }
