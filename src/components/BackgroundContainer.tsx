c780b4c8529e448c16517b5c0440b59f45540c12
Lucas Redding <lre61@uclive.ac.nz>
2024-06-28 18:11:36 +1200

    Added varied backgrounds and changed background opacity

--- src/components/BackgroundContainer.tsx
+++ src/components/BackgroundContainer.tsx
@@ -5,19 +5,31 @@ const BackgroundContainer = ({ children, }: Readonly<{ children: React.ReactNode
   return (
     <Box
       sx={{
-        backgroundImage: 'url(/back.png)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
+        position: 'relative',
         minHeight: 'calc(100vh - 60px)',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'flex-start',
         marginTop: '60px',
-        paddingTop: '0vh'
+        paddingTop: '5vh',
       }}
     >
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '0',
+          left: '0',
+          width: '100%',
+          height: '100%',
+          backgroundImage: 'url(/back_v1.png)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          opacity: 0.7,
+          zIndex: -1,
+        }}
+      />
       {children}
     </Box>
   );
