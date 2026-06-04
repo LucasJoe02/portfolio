c01b02c845a4c678e2f2ad901d1d556e0e497dbe
Lucas Redding <fishlikerlucas@gmail.com>
2026-06-04 11:27:28 +0100

    Add pixelated ray tracer thumbnail to portfolio card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import PostList from "@/components/PostList";
 
 const items = [
   { id: 1, title: 'Boids', description: '', img: '/back_v2.png', path: '/boids'},
-  { id: 2, title: 'Ray Tracer', description: '', img: '/green.jpg', path: '/raytracer' },
+  { id: 2, title: 'Ray Tracer', description: '', img: '/raytracer_pixel.png', path: '/raytracer' },
   { id: 3, title: '*Coming Soon*', description: '', img: '/yellow.jpg', path: '/' },
 ];
 
